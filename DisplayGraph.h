229eb3d44400f106a8cc6215bf11e9b9a8232674
Muhammad Nizami <nizami_muhammad@yahoo.co.id>
2015-04-04 08:25:59 +0700

    tambah credit

--- DisplayGraph.h
+++ DisplayGraph.h
@@ -1,6 +1,9 @@
 //file:DisplayGraph.h
 #include<vector>
 
+
+//tampilan graf dilakukan dengan bantuan shell, gnome-open, dan graphviz dot.
+
 void displayGraph(std::vector<std::vector<float> > Graph);
 
 void displayGraph(std::vector<std::vector<float> > Graph, std::vector<int> circuit);
